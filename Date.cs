1256ca0759db86a0b61d52b7aef4ea4f7ff92d6f
veelenar <kapcer.priv@gmail.com>
2016-06-05 11:45:56 +0200

    Drobne poprawki w interfejsach

--- Date.cs
+++ Date.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_Manager_CSharp_WPF
+﻿namespace Task_Manager_CSharp_WPF
 {
     public class Date
     {
--- Task.cs
+++ Task.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Task_Manager_CSharp_WPF
+﻿namespace Task_Manager_CSharp_WPF
 {
     public class Task
     {
@@ -15,26 +9,6 @@ namespace Task_Manager_CSharp_WPF
         public virtual string GetEndingDate()
         {
             return "data";
-        }
-    }
-
-    public class CreateTask : Task
-    {
-        public CreateTask() { }
-
-        public CreateTask(string taskname, string surname, string name, int day, int month, int year)
-        {
-            this.TaskName = taskname;
-            this.EndingDate.Day = day.ToString();
-            this.EndingDate.Month = month.ToString();
-            this.EndingDate.Year = year.ToString();
-            this.ResponsiblePerson.Name = name;
-            this.ResponsiblePerson.Surname = surname;
-        }
-
-        public override string GetEndingDate()
-        {
-            return this.EndingDate.Day + "." + this.EndingDate.Month + "." + this.EndingDate.Year;
         }
     }
 }
